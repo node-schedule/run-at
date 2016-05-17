a4e7dc278157a98c1ed2710948bb66982129ec02
Timothy Cyrus <tcyrus@users.noreply.github.com>
2016-05-17 13:27:34 -0400

    Update index.js
    Return the result of `setTimeout()`, so that user can call `clearTimeout()`
    using the result

--- src/index.js
+++ src/index.js
@@ -16,5 +16,5 @@ import date from 'date.js'
 export default function(str, fn) {
   const offset = date(str) - new Date
   if (!offset) throw `did not recognize '${str}'`
-  setTimeout(fn, offset)
+  return setTimeout(fn, offset)
 }
